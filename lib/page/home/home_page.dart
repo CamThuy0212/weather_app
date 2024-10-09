01a422744df28bb192e5a2561504edcfcbf5f044
CamThuy0212 <camthuy0212@gmail.com>
2024-10-09 17:29:48 +0700

    add UI home page

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/page/home/widgets/home_temperature.dart';
+import 'package:weather_app/page/home/widgets/home_weather_icon.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xff1d6cf3), Color(0xff19d2fe)],
+          ),
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            HomeWeatherIcon(),
+            HomeTemperature(),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/page/home/widgets/home_location.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeLocation extends StatelessWidget {
+  const HomeLocation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
